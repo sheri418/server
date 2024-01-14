0cd21ad9aa135058c02766406d81a27131c8b2bf
sheri418 <sherazgoraya418@gmail.com>
2024-01-14 16:39:41 +0500

    update

--- utils/errorHandling.ts
+++ utils/errorHandling.ts
@@ -1,3 +1,13 @@
-class errorHandler extends Error{
-    
+class ErrorHandler extends Error{
+    statusCode:Number;
+
+    constructor(message:any,statusCode:Number){
+        super(message);
+        this.statusCode = statusCode;
+
+        Error.captureStackTrace(this,this.constructor);
+
+    }
 }
+
+module.exports = ErrorHandler;
